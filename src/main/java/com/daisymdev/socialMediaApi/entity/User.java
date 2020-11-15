90c9dcf5dc8128981a116ed32ee931af951382a4
Daisy Murillo <daisy21murillo@gmail.com>
2020-11-15 13:55:53 -0700

    annotations for classes complete, successful connection to db

--- src/main/java/com/daisymdev/socialMediaApi/entity/User.java
+++ src/main/java/com/daisymdev/socialMediaApi/entity/User.java
@@ -2,15 +2,37 @@ package com.daisymdev.socialMediaApi.entity;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
 public class User {
     private Long id;
     private String username;
     private String profilePictureUrl;
+
+    @JsonIgnore
     private Set<User> following;
     private String password;
+
+    @JsonIgnore
     private Set<Post> posts;
+
+    @JsonIgnore
     private Set<Comment> comments;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
@@ -27,14 +49,17 @@ public class User {
         this.username = username;
     }
 
+    @JsonIgnore
     public String getPassword() {
         return password;
     }
 
+    @JsonProperty
     public void setPassword(String password) {
         this.password = password;
     }
 
+    @OneToMany(mappedBy = "user")
     public Set<Post> getPosts() {
         return posts;
     }
@@ -43,6 +68,7 @@ public class User {
         this.posts = posts;
     }
 
+    @OneToMany(mappedBy = "user")
     public Set<Comment> getComments() {
         return comments;
     }
@@ -51,6 +77,8 @@ public class User {
         this.comments = comments;
     }
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "following", joinColumns = @JoinColumn(name = "userId", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "followingId", referencedColumnName = "id"))
     public Set<User> getFollowing() {
         return following;
     }

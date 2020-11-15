6a7c792d1243465ca8a0a39604e7be743f6e2efa
Daisy Murillo <daisy21murillo@gmail.com>
2020-11-14 23:16:17 -0700

    comment entity complete

--- src/main/java/com/daisymdev/socialMediaApi/entity/Comment.java
+++ src/main/java/com/daisymdev/socialMediaApi/entity/Comment.java
@@ -1,6 +1,52 @@
 package com.daisymdev.socialMediaApi.entity;
 
+import java.util.Date;
+
 public class Comment {
 
+    private Long id;
+    private String content;
+    private Date date;
+    private User user;
+    private Post post;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }

8f5ba1b91bbf7956fd8e3656e7b9884905057fa8
Daisy Murillo <daisy21murillo@gmail.com>
2020-11-16 08:38:27 -0700

    following class complete

--- src/main/java/com/daisymdev/socialMediaApi/view/FollowingJava.java
+++ src/main/java/com/daisymdev/socialMediaApi/view/FollowingJava.java
@@ -1,4 +1,18 @@
 package com.daisymdev.socialMediaApi.view;
 
 public class FollowingJava {
+
+    private Set<User> following;
+
+    public Following(User user) {
+        following = user.getFollowing();
+    }
+
+    public Set<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<User> following) {
+        this.following = following;
+    }
 }
